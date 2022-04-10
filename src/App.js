e7162ca68d28412eb4c701486d2c572d32a0f450
Juan Manuel Stella <0x86jm14sMb94@gmail.com>
2022-04-10 13:43:30 +0200

    Animation: pokemon container opens up and sets the screen for the pokemon to appear.

--- src/App.js
+++ src/App.js
@@ -12,11 +12,17 @@ function App() {
   return (
     <div className="App">
       <div className="pokedex_box">
+          <div className="pokemon_screen_top"> 
+          </div>
+          <div className="pokemon_screen_bottom"> 
+          </div>
           <div className="pokemon_screen" id="animationGlitchSquare1"> 
           </div>
           <div className="pokemon_screen" id="animationGlitchSquare2"> 
           </div>
           <div className="pokemon_screen" id="animationGlitchSquare3"> 
+          </div>
+          <div className="pokemon_screen static"> 
           </div>
           <div className="pokemon_background"> 
           </div>
